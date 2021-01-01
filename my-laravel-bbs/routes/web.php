c453467c5c1f87270cba9544f92f0a17f58f672e
Yuta Hosoya <53441044+hony0@users.noreply.github.com>
2021-01-02 01:29:27 +0900

    implement add post & add comment func (#4)
    Co-authored-by: 細谷祐太 <yuta@hony0-pc.local>

--- my-laravel-bbs/routes/web.php
+++ my-laravel-bbs/routes/web.php
@@ -12,3 +12,5 @@
 */
 
 Route::get('/', 'PostsController@index')->name('top');
+Route::resource('posts', 'PostsController', ['only' => ['create', 'store', 'show']]);
+Route::resource('comments', 'CommentsController', ['only' => ['store']]);
