7c31570d61e8d49fbce6359382c7867c6fbb5644
Yuta Hosoya <53441044+hony0@users.noreply.github.com>
2021-01-02 03:01:26 +0900

    Implement detail screen (#5)
    * implement add post & add comment func
    
    * implement edit & delete post func
    
    Co-authored-by: 細谷祐太 <yuta@hony0-pc.local>

--- my-laravel-bbs/app/Http/Controllers/PostsController.php
+++ my-laravel-bbs/app/Http/Controllers/PostsController.php
@@ -8,7 +8,8 @@ use App\Post;
 class PostsController extends Controller
 {
     public function index() {
-        $posts = Post::orderBy('created_at', 'desc')->get();
+        // $posts = Post::orderBy('created_at', 'desc')->paginate(10);
+        $posts = Post::with(['comments'])->orderBy('created_at', 'desc')->paginate(10);
         return view('posts.index', ['posts' => $posts]);
     }
 
@@ -31,5 +32,35 @@ class PostsController extends Controller
         return view('posts.show', [
             'post' => $post,
         ]);
+    }
+
+    public function edit($post_id) {
+        $post = Post::findOrFail($post_id);
+        return view('posts.edit', [
+            'post' => $post,
+        ]);
+    }
+
+    public function update($post_id, Request $request) {
+        $params = $request->validate([
+            'title' => 'required|max:50',
+            'body' => 'required|max:2000',
+        ]);
+
+        $post = Post::findOrFail($post_id);
+        $post->fill($params)->save();
+
+        return redirect()->route('posts.show', ['post' => $post]);
+    }
+
+    public function destroy($post_id) {
+        $post = Post::findOrFail($post_id);
+
+        \DB::transaction(function () use ($post) {
+            $post->comments()->delete();
+            $post->delete();
+        });
+
+        return redirect()->route('top');
     }
 }
